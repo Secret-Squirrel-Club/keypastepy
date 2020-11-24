987461e1084263c63c20a7de4d232f3aa3ca94e6
Nicholas Shorter <nickshorter@Nicholass-MacBook-Pro.local>
2020-11-24 16:39:30 -0600

    Added connect to db

--- keypaste/keypaste.py
+++ keypaste/keypaste.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import sqlite3
+import logging
+from sqlite3.dbapi2 import Error
 from keypaste.command import Command
 from keypaste.paste import Paste
 
@@ -8,4 +10,14 @@ class Keypaste():
         self.command = command
         self.paste = paste
 
+    def connect_to_db(self):
+        db = None
+        try:
+            db = sqlite3.connect('~/.config/keypaste/keypaste.db')
+            logging.debug(f'{sqlite3.version_info}')
+        except Error as Err:
+            logging.fatal(Err)
+        finally:
+            if db:
+                db.close()
     
